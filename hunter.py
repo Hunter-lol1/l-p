b12a55f84e271833d2345aa2a5e06cfc9b74d9c1
Hunter-lol1 <158170800+Hunter-lol1@users.noreply.github.com>
2024-03-26 19:25:42 -0300

    Add files via upload

---
+++ hunter.py
@@ -0,0 +1,21 @@
+numero = int(input('Digite um número: '))
+
+if numero % 2 == 0:
+    print('O número é par!')
+else:
+    print('O número é impar!')
+
+====================================================================================================
+
+idade = int(input('Digite sua idade: '))
+
+if idade <= 12:
+    print('Você é criança!')
+elif idade < 18:
+    print('Você é adolescente!')
+if idade <= 24:
+    print('Já pode ir preso em!')
+if idade <= 25:
+    print('Ser adulto é triste né? se fodeu!')
+if idade <= 100:
+    print('Ta velho em, MORRA, MORRA, MORRA, MORRA')
