f4a6005afd98990ec35d14aa0b200d51216be6f5
Hunter-lol1 <158170800+Hunter-lol1@users.noreply.github.com>
2024-03-27 18:08:30 -0300

    Update hunter.py login

--- hunter.py
+++ hunter.py
@@ -19,3 +19,16 @@ if idade <= 25:
     print('Ser adulto é triste né? se fodeu!')
 if idade <= 100:
     print('Ta velho em, MORRA, MORRA, MORRA, MORRA')
+
+====================================================================================================
+
+usuario_correto = 'canabis'
+senha_correta = '123'
+
+usuario = input('Digite seu nome de usúario: ')
+senha = input('Digite sua senha: ')
+
+if usuario == usuario_correto and senha == senha_correta:
+    print('Login correto, bem vindo!')
+else:
+    print('Login incorreto, tente novamente!')
